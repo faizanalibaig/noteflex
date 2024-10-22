464a04052f38878ffc3a1393ef99ad1a462e17ce
faizanalibaig <riazfiaz6181@gmail.com>
2024-10-22 12:14:26 +0500

    noteflex

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <section className="w-screen h-screen bg-[#005CE6] flex flex-col justify-end px-6 py-8 sm:px-10 sm:py-14 gap-1">
+      <h1 className="text-3xl text-white font-bold font-main">noteflex</h1>
+      <div className="bg-white rounded-sm w-full h-[2px]" />
+    </section>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    fontFamily: {
+      main: ['EB Garamond', 'sans-serif'],
+      secondary: ['DM Sans', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
