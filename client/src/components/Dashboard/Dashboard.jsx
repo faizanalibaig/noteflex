f139aa14c37692925e771977db96c7cc64805d4f
faizanalibaig <riazfiaz6181@gmail.com>
2025-04-04 22:03:22 +0500

    Refractored Dashboard code

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,26 +1,40 @@
 import React from 'react';
 
+const data = [
+  {
+    title: 'How to create a frontend applications?',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia molestiae veniam velit praesentium maxime quis aut! Dignissimos omnis commodi maxime.',
+  },
+  {
+    title: 'How to create a frontend applications?',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia molestiae veniam velit praesentium maxime quis aut! Dignissimos omnis commodi maxime.',
+  },
+];
+
 function Dashboard() {
   return (
-    <main className='px-12 py-5'>
-      <div className='w-[300px] h-[250px] flex flex-col justify-between bg-black/[0.03] rounded-[6px] p-4'>
-        <div>
-          <h1 className='font-medium text-lg font-main'>
-            How to create a frontend applications?
-          </h1>
-          <p className='font-secondary text-sm mt-3'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
-            molestiae veniam velit praesentium maxime quis aut! Dignissimos
-            omnis commodi maxime.
-          </p>
-        </div>
+    <main className='px-12 py-5 flex gap-4 flex-wrap'>
+      {data.map((d, i) => {
+        return (
+          <div
+            key={i}
+            className='w-[300px] h-[250px] flex flex-col justify-between bg-black/[0.03] rounded-[6px] p-4'
+          >
+            <div>
+              <h1 className='font-medium text-lg font-main'>{d.title}</h1>
+              <p className='font-secondary text-sm mt-3'>{d.description}</p>
+            </div>
 
-        <div>
-          <h3 className='font-secondary text-sm underline underline-offset-2 cursor-pointer'>
-            view more
-          </h3>
-        </div>
-      </div>
+            <div>
+              <h3 className='font-secondary text-sm underline underline-offset-2 cursor-pointer'>
+                view more
+              </h3>
+            </div>
+          </div>
+        );
+      })}
     </main>
   );
 }
