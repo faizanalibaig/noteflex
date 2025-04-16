7f5f228b0454613615627560d895e42a917f5609
faizanalibaig <riazfiaz6181@gmail.com>
2025-04-16 18:23:57 +0500

    Remove Auth for a moment from Routes

--- client/src/routes/NoteRoutes.jsx
+++ client/src/routes/NoteRoutes.jsx
@@ -5,16 +5,16 @@ import Dashboard from '../components/Dashboard/Dashboard';
 import DashboardLayout from '../layouts/DashboardLayout';
 import HomeLayout from '../layouts/HomeLayout';
 import AuthLayout from '../layouts/AuthLayout';
-import { AuthSignup, AuthLogin } from '../components/Auth/index.js';
+// import { AuthSignup, AuthLogin } from '../components/Auth/index.js';
 
 function NoteRoutes() {
   return (
     <Routes>
       <Route path='/' element={<HomeLayout />} />
-      <Route path='auth' element={<AuthLayout />}>
+      {/* <Route path='auth' element={<AuthLayout />}>
         <Route path='signup' element={<AuthSignup />} />
         <Route path='login' element={<AuthLogin />} />
-      </Route>
+      </Route> */}
       <Route path='dashboard' element={<DashboardLayout />}>
         <Route index element={<Dashboard />} />
         <Route path='create-note' element={<CreateNote />} />
