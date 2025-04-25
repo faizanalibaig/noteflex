c5c3c6a3019ec9ca16dc888c6c0103770e680e6a
faizanalibaig <riazfiaz6181@gmail.com>
2025-04-25 23:20:41 +0500

    Note routes setup complete and note title separate functionality comment out

--- server/routes/note.route.js
+++ server/routes/note.route.js
@@ -5,4 +5,9 @@ const Note = require('../controllers/note.controller');
 const verifytoken = require('../middleware/verify-token');
 
 router.post('/create', verifytoken, Note.create);
+router.get('/find/:id', verifytoken, Note.retrieve);
+router.post('/find', verifytoken, Note.retrieveAll);
+router.patch('/update', verifytoken, Note.update);
+router.delete('/delete', verifytoken, Note.delete);
+
 module.exports = router;
