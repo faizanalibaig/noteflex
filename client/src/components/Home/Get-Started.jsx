241043557e2929aeb926b3991378c5697cd589b4
faizanalibaig <riazfiaz6181@gmail.com>
2025-04-26 16:32:58 +0500

    Get Started & Home Header components complete responsive now - used dvh for mobile screen height

--- client/src/components/Home/Get-Started.jsx
+++ client/src/components/Home/Get-Started.jsx
@@ -8,20 +8,20 @@ import { Link } from 'react-router';
 
 function GetStarted() {
   return (
-    <section className='h-[90%] relative'>
+    <section className='h-[92%] sm:h-[91%] lg:h-[90%] relative'>
       <section className='h-full flex justify-center items-center px-5 sm:px-28 lg:px-40 xl:px-60'>
-        <div className='flex flex-col justify-center items-center gap-5 mb-32'>
+        <div className='flex flex-col justify-center items-center gap-4 lg:gap-5 mb-32 sm:mb-28 xl:mb-32'>
           <div className='bg-[#005CE6]/5 border border-[#005CE6]/10 w-auto h-8 sm:h-[38px] rounded-full px-4 sm:px-6  text-[#005CE6] flex justify-center items-center gap-2'>
             <p className=' font-secondary text-xs lg:text-sm xl:text-base font-medium'>
-              write your daily important notes
+              Write your daily important notes
             </p>
-            <MdNote className='text-lg mt-[1px] hidden sm:block' />
+            <MdNote className='text-lg mt-[1px]' />
           </div>
 
           <h1 className='font-bold text-3xl sm:text-4xl lg:text-5xl xl:text-7xl font-main'>
             Get Started With Noteflex
           </h1>
-          <p className='text-center font-secondary text-xs sm:text-sm xl:text-base'>
+          <p className='text-center leading-5 font-secondary text-sm sm:text-base'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
             harum iure facere atque voluptate iste veritatis expedita hic
             praesentium ad vel corporis ex aliquid cumque nihil odit
@@ -32,6 +32,7 @@ function GetStarted() {
           </p>
         </div>
       </section>
+
       <div className='absolute bottom-10 left-5 sm:left-12'>
         <Link
           to='https://github.com/faizanalibaig/noteflex'
@@ -39,7 +40,7 @@ function GetStarted() {
           className='flex items-center gap-2 outline-none font-secondary font-medium'
         >
           <span>
-            <FaGithubAlt className='sm::text-base xl:text-xl' />
+            <FaGithubAlt className='text-lg xl:text-xl' />
           </span>
         </Link>
       </div>
@@ -47,7 +48,7 @@ function GetStarted() {
       <div className='absolute bottom-10 right-5 sm:right-12'>
         <Link
           to='/auth/login'
-          className='flex items-center gap-2 outline-none font-secondary font-medium text-xs sm:text-sm xl:text-base'
+          className='flex items-center gap-2 outline-none font-secondary font-medium text-sm sm:text-base'
         >
           <span>get started</span> <BsArrowBarRight />
         </Link>
