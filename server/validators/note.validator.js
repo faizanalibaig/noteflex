30d30fd22af828b03aedcd3135f1a8672a6d5561
faizanalibaig <riazfiaz6181@gmail.com>
2025-05-20 20:18:49 +0500

    Note Joi validator updated according to recent changes in the schema

--- server/validators/note.validator.js
+++ server/validators/note.validator.js
@@ -1,8 +1,12 @@
 const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
 
 function ValidateNote(note) {
   const schema = Joi.object({
     title: Joi.string().min(6).max(512).required(),
+    descripiton: Joi.string().min(10).max(5024).required(),
+    user: Joi.objectId().required(),
+    tags: Joi.array().items(Joi.string()).max(5),
   });
 
   return schema.validate(note);
